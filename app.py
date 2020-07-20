39ce5f870a92fe6074ff6a48ab7bf9f28ebfe18a
mariakesa <maria.kesa@gmail.com>
2020-07-20 23:55:33 +0200

    Brain rendering

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ import numpy as np
 from streamlit import caching
 import pandas as pd
 
+# Import variables
+from brainrender import * # <- these can be changed to personalize the look of your renders
+
+# Import scene class
+from brainrender.scene import Scene
+
+import sys
+sys.path.append('../')
+import os
+
+# Set up VTKPLOTTER to work in Jupyter notebooks
+from vtkplotter import *
 
 all_data_path='/media/maria/DATA1/Documents/NeuroMatchAcademy2020_dat/unzipped_files'
 
@@ -26,3 +38,21 @@ brain_areas=get_brain_areas(all_data_path)
 brain_option = st.selectbox('Which circuit to visualize?', brain_areas)
 
 st.write('You selected:', brain_option)
+
+import brainrender
+brainrender.SHADER_STYLE = 'cartoon'
+from brainrender.scene import Scene
+
+# Create a scene
+scene = Scene()
+
+scene.add_brain_regions([brain_option], alpha=.15)
+
+#scene.render()
+vp = Plotter(axes=0)
+vp.show(scene.get_actors(), viewup=(10, 0.7, 0))
+
+scene.close()
+
+#vp = Plotter(axes=0)
+#vp.show(tutorial_scene.get_actors(), viewup=(10, 0.7, 0))
