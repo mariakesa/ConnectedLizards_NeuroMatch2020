d0594f11aeac5234e45e7b9de2315cba677b5088
mariakesa <maria.kesa@gmail.com>
2020-07-28 18:22:40 +0200

    Fix functions

--- SubspacePipeline.py
+++ SubspacePipeline.py
@@ -41,23 +41,17 @@ def ridge_regression(train_inputs,train_outputs,test_inputs,test_outputs,regular
 
     return from_front_to_mid_reg
 
-def ridge_behavior(from_front_to_mid_reg,pca_frontal,pcs_front,front_test,pcs_front_test,pcs_beh,pcs_beh_test,pcs_mid_test,dimensions):
-    ridge_beh=Ridge()
-    ridge_beh.fit(pcs_beh,(from_front_to_mid_reg.coef_[dimensions[0],:]@(pcs_front.T)).T)
-    pred_beh=ridge_beh.coef_@pcs_beh_test.T
-    actual=(from_front_to_mid_reg.coef_@(pca_frontal.transform(front_test).T))[dimensions[0],:]
+def ridge_behavior(from_front_to_mid_reg,pca_frontal,pcs_front,front_test,pcs_front_test,pcs_beh,pcs_beh_test,pcs_mid_test,dimensions,alpha=0.1):
+    ridge_beh=Ridge(alpha)
+    ridge_beh.fit((from_front_to_mid_reg.coef_[dimensions[0],:]@(pcs_front.T)).T,pcs_beh)
+    pred_beh=ridge_beh.coef_@from_front_to_mid_reg.coef_[dimensions[0],:]@(pcs_front_test.T)
+    actual=pcs_beh_test.T
     for j in range(0,dimensions[0].shape[0]):
         print(j)
         print('corr coef, dim=' + str(dimensions[0][j])+':', np.corrcoef(pred_beh[j,:],actual[j,:])[0,1])
         plt.plot(pred_beh[j,:],label='prediction')
         plt.plot(actual[j,:],label='original')
-        plt.show()
-    for j in range(0,pcs_mid_test.shape[1]):
-        print(j)
-        print('corr matrix between midbrain pcs and predictions from behavior')
-        corrs_with_midbrain=np.corrcoef(pcs_mid_test[:,j],pred_beh[:2,:])
-        print(corrs_with_midbrain)
-        sns.heatmap(corrs_with_midbrain)
+        plt.legend()
         plt.show()
 
 def variance_explained(dat,prediction):
@@ -65,15 +59,13 @@ def variance_explained(dat,prediction):
     natural_variance=(dat-np.mean(dat))**2
     return 1-residuals.sum()/natural_variance.sum()
 
-def ridge_nullspace(projection_onto_nullspace_train,projection_onto_nullspace_test,pcs_beh,pcs_beh_test,alpha=0.1):
+def ridge_nullspace(projection_onto_nullspace_train,projection_onto_nullspace_test,pcs_beh,pcs_beh_test,alpha=0.01):
     ridge_null=Ridge(alpha)
-    ridge_null.fit(pcs_beh,projection_onto_nullspace_train.T)
-    pred_beh=ridge_null.coef_@pcs_beh_test.T
-    actual=projection_onto_nullspace_test
-
-    for j in range(0,pred_beh.shape[0]):
-        print(j)
-        print('corr coef: ', np.corrcoef(pred_beh[j,:],actual[j,:])[0,1])
+    ridge_null.fit(projection_onto_nullspace_train.T,pcs_beh)
+    pred_beh=ridge_null.coef_@projection_onto_nullspace_test
+    actual=pcs_beh_test.T
+    for j in range(0,actual.shape[0]):
+        print('corr coef, dim=' + str(j)+':', np.corrcoef(pred_beh[j,:],actual[j,:])[0,1])
         plt.plot(pred_beh[j,:],label='prediction')
         plt.plot(actual[j,:],label='original')
         plt.legend()
